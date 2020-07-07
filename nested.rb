6f2bd167c8ac6b74168a5e853270f8ed8e718503
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-07 21:01:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,7 +126,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     } 
-     puts programmer_hash[:dennis_ritchie][:languages]= :languages.push("Assembly")
+     puts programmer_hash[:dennis_ritchie][:languages]= languages.push("Assembly")
       puts programmer_hash[dennis_ritchie]
 end
 

af8c61ca85d240f60250672286a0d65f19e019f3
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-07 19:52:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,6 +76,7 @@ def adding_matz
       }
     }
     programmer_hash[:yukihiro_matsumoto] 
+    programmer
     
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {

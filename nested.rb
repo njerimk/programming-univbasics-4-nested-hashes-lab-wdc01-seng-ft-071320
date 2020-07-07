2e240703a9c62fd6eed2fe63a3d78cb47fd6556d
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-07 20:07:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,7 +76,6 @@ def adding_matz
       }
     }
     puts programmer_hash[:yukihiro_matsumoto] =>[:known_for =>"Ruby"] + [:languages => "LISP" "C"]
-    puts programmer_hash
     
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {

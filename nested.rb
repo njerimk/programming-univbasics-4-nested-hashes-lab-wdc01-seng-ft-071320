4659b52a58b181a8aa57792d92b6bf0cc9b3e40d
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-07 20:33:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
     programmer_hash[:alan_kay]=[:known_for => "GUI"]
 
 end
-changing_alan
+puts changing_alan
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array

37df5dbf9d9d37cbcfd684ac827d9180f9379109
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-07 19:58:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -73,7 +73,7 @@ def adding_matz
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
-      }programmer_hash[:yukihiro_matsumoto]
+      }
     }
     puts programmer_hash
     
